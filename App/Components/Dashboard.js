1c4e9cdf4f8f635c7c176a66de5776cbd42af307
rickyPanzer <richardrpanzer@gmail.com>
2016-01-13 18:50:22 -0500

    lessons up to 8 on egghead tutorials

---
+++ App/Components/Dashboard.js
@@ -0,0 +1,80 @@
+import React, {
+  Text,
+  View,
+  Component,
+  StyleSheet,
+  Image,
+  TouchableHighlight
+} from 'react-native';
+
+var styles = StyleSheet.create({
+  container: {
+    marginTop: 65,
+    flex: 1
+  },
+  image: {
+    height: 350
+  },
+  buttonText: {
+    fontSize: 24,
+    color: 'white',
+    alignSelf: 'center'
+  }
+});
+
+class Dashboard extends Component{
+  makeBackground(btn){
+    var obj = {
+      flexDirection: 'row',
+      alignSelf: 'stretch',
+      justifyContent: 'center',
+      flex: 1
+    }
+
+    if (btn === 0){
+      obj.backgroundColor = '#48BBEC';
+    } else if (btn === 1){
+      obj.backgroundColor = '#E77AAE';
+    } else if (btn === 2){
+      obj.backgroundColor = '#758BF4';
+    }
+    return obj;
+  }
+  goToProfile(){
+    console.log('Going to Profile Page');
+  }
+  goToRepos(){
+    console.log('Going to Repos');
+  }
+  goToNotes(){
+    console.log('Going to Notes');
+  }
+
+  render(){
+    return(
+      <View style={styles.container}>
+        <Image source={{uri: this.props.userInfo.avatar_url}} style={styles.image} />
+        <TouchableHighlight
+          style={this.makeBackground(0)}
+          onPress={this.goToProfile.bind(this)}
+          underlayColor='#88D4F5'>
+          <Text style={styles.buttonText}> View Profile </Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+          style={this.makeBackground(1)}
+          onPress={this.goToRepos.bind(this)}
+          underlayColor='#88D4F5'>
+          <Text style={styles.buttonText}> View Profile </Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+          style={this.makeBackground(2)}
+          onPress={this.goToNotes.bind(this)}
+          underlayColor='#88D4F5'>
+          <Text style={styles.buttonText}> View Profile </Text>
+        </TouchableHighlight>
+      </View>
+    )
+  }
+}
+
+module.exports = Dashboard;
